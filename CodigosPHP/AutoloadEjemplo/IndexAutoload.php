2554a57488e998109a86c962e6335222247147da
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-10-27 17:16:01 +0200

    Onload Example

--- CodigosPHP/AutoloadEjemplo/IndexAutoload.php
+++ CodigosPHP/AutoloadEjemplo/IndexAutoload.php
@@ -1,9 +1,31 @@
 <html>
-    <head><title>Autoload Example</title></head>
-    <body>
-        <div style="margin:20%  ">
-            <h3>Ejemplo de Carga de Clases </h3>
-            <p>En esta ocasión cargaremos la clase "Clases"</p>
-        </div>
-    </body>
+
+<head>
+    <title>Autoload Example</title>
+</head>
+
+<body>
+    <div style="margin:20%  ">
+        <h3>Ejemplo de Carga de Clases </h3>
+        <p>En esta ocasión cargaremos la clase "Clases"</p>
+        <form action="ejemplo.php">
+            <p>Nombre: <input type="text" name="nombre"></p>
+            <p><input type="submit" value="Enviar"></p>
+        </form>
+        <?php
+        $clase = $_REQUEST["nombre"];
+        function buscaClass($clase)
+        {
+            require "$clase.php";
+        }
+
+        spl_autoload_register('buscaClass');
+
+            $ejemplo1 = new Clases("KermitFanboy","PHP-Object-Instance");
+
+            echo "Hola $ejemplo1";
+        ?>
+    </div>
+</body>
+
 </html>
