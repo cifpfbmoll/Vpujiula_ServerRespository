d1dcc4a01262d590fabfef74d1f5a02f0e88164c
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-10-27 16:59:53 +0200

    Class example

---
+++ CodigosPHP/AutoloadEjemplo/Clases.php
@@ -0,0 +1,12 @@
+<?php   
+
+class Clases {
+
+    public $Nombre;
+    public $Tipo;
+
+    function __construct($Nombre, $Tipo) {
+        $this->Nombre = $Nombre;
+        $this->Tipo = $Tipo;}
+};
+?>
