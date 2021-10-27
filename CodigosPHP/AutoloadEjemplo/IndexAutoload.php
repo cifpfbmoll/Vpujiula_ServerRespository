7274e62be268e830db3d8b9d76766210d2275512
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-10-27 16:48:15 +0200

    Index Autoload

---
+++ CodigosPHP/AutoloadEjemplo/IndexAutoload.php
@@ -0,0 +1,9 @@
+<html>
+    <head><title>Autoload Example</title></head>
+    <body>
+        <div style="margin:20%  ">
+            <h3>Ejemplo de Carga de Clases </h3>
+            <p>En esta ocasión cargaremos la clase "Clases"</p>
+        </div>
+    </body>
+</html>
