fe41c4533388a9ebd747955c752e6c0137a9a2fe
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-10-27 17:22:46 +0200

    Fixv3

--- CodigosPHP/AutoloadEjemplo/Clases.php
+++ CodigosPHP/AutoloadEjemplo/Clases.php
@@ -4,7 +4,10 @@ class Clases {
 
     public $Nombre;
     public $Tipo;
-
+    public function __toString()
+    {
+      return $this->Nombre . ' '. $this->Tipo;
+    }
     function __construct($Nombre, $Tipo) {
         $this->Nombre = $Nombre;
         $this->Tipo = $Tipo;}
--- CodigosPHP/AutoloadEjemplo/IndexAutoload.php
+++ CodigosPHP/AutoloadEjemplo/IndexAutoload.php
@@ -8,10 +8,6 @@
     <div style="margin:20%  ">
         <h3>Ejemplo de Carga de Clases </h3>
         <p>En esta ocasión cargaremos la clase "Clases"</p>
-        <form action="ejemplo.php">
-            <p>Nombre: <input type="text" name="nombre"></p>
-            <p><input type="submit" value="Enviar"></p>
-        </form>
         <?php
         $clase = "Clases";
         function buscaClass($clase)
@@ -23,7 +19,7 @@
 
             $ejemplo1 = new Clases("KermitFanboy","PHP-Object-Instance");
 
-            echo "Hola" . $ejemplo1;
+            echo $ejemplo1;
         ?>
     </div>
 </body>
