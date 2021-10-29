e3b4de871b6a5d539bf467b2a3dcd7d879a28c8b
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-10-29 17:28:45 +0200

    Commit Example

--- CodigosPHP/Capitulo2/ClasePrivadaConGetter/PuppetClass.php
+++ CodigosPHP/Capitulo2/ClasePrivadaConGetter/PuppetClass.php
@@ -1,28 +1,47 @@
-<?php
+<html>
 
-class Puppet{
+<head>
+    <Title>Example</Title>
+</head>
 
-    public $Name;
-    protected $ID;
-    private $Age;
-    public $Surname;
+<body>
+<h2>Ejemplo de Uso:</h2>
+    <?php
+
+    class Puppet
+    {
+
+        public $Name;
+        protected $ID;
+        private $Age;
+        public $Surname;
 
 
-    function __construct($Name, $ID, $Age, $Surname) {
-        $this->Name = $Name;
-        $this->ID = $ID;
-        $this->Age = $Age;
-        $this->Surname = $Surname;
+        function __construct($Name, $ID, $Age, $Surname)
+        {
+            $this->Name = $Name;
+            $this->ID = $ID;
+            $this->Age = $Age;
+            $this->Surname = $Surname;
+        }
+
+        public function getName()
+        {
+            return $this->Name;
+        }
+        public function getPrivateAge()
+        {
+            return $this->Age;
+        }
+
     }
 
-    public function getName(){
-        return $this-> Name;
-    }
-    public function getPrivateAge(){
-        return $this->Age;
-    }
+    ?>
+    <?php 
+    $ranaGustavo = new Puppet("KermitW",7,19,"TheFrog");
+    echo $ranaGustavo->getPrivateAge();
+    ?>
     
-}
+</body>
 
-
-?>
+</html>
