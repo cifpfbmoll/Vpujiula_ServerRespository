86d4580d722e5d16de3595638eadc0f248d26fef
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-11-03 16:33:08 +0100

    Fix css

--- CodigosPHP/Capitulo2/AgendaContactos/AgendaContactosMEJORADA.php
+++ CodigosPHP/Capitulo2/AgendaContactos/AgendaContactosMEJORADA.php
@@ -1,21 +1,83 @@
-<?php session_start();?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <!-- Cookies - session_start() antes de HTML  -->
 <!-- http://idesweb.es/proyecto/proyecto-prac09-php-cookies-sesiones -->
 <!--  https://www.w3schools.com/php/php_cookies.asp -->
-<head></head>
+
+<head>
+    <title>Agenda de Contactos::</title>
+    <style>
+        .CenterContent{
+            text-align: center;
+        }
+    </style>
+</head>
+
 <body>
     <?php
-    $cookie_name = "ValentineCookie";
+
+    //COOKIES INITIAL TRY:-----------------------------------------------------------------------
+    /* $cookie_name = "ValentineCookie";
     setcookie("$cookie_name" , "Test about when a cookie starts", time() + 3600, '/');
     if(!isset($_COOKIE[$cookie_name])) {
         echo "Cookie named '" . $cookie_name . "' is not set!";
       } else {
         echo "Cookie '" . $cookie_name . "' is set!<br>";
         echo "Value is: " . $_COOKIE[$cookie_name];
-      }
-    
-    echo ini_get("session.session_save_path");
+      } 
+      ---------------------------------------------------------------------------------------------*/
     ?>
+
+    <div class="CenterContent">
+        <h3>Agenda de Contactos (NEW):</h3>
+        <?
+        if (isset($_GET['lista'])) {
+            $lista = $_GET['lista'];
+        } else {
+            $lista = [];
+        }
+        //Cojemos Los datos del envio "GET"
+        if (isset($_GET['submit'])) {
+            //$array["nombre del primero"] = $array[1];
+            //unset($array[1]);
+            if (trim($_GET['name']) != "" && $_GET['tel'] != "") {
+                $lista[$_GET['name']] = $_GET['tel']; //Asignamos el telefono al Nombre , asi podremos llamar a la posición sin depender de *N*
+            } else {
+                //Control de parámetros : Comprobación | Eliminación
+                if (empty($_GET['name'])) {
+                    echo "El Campo Nombre debe rellenarse ";
+                } else if (isset($lista[$_GET['name']]) !== true) {
+                    echo "No exixte en la Lista Telefónica , Introduzca primero el Valor";
+                } else {
+                    echo "El contacto -->" . $_GET['name'] . "Fue Eliminado";
+                    unset($lista[$_GET['name']]);
+                }
+            }
+        }
+        ?>
+        <div style="justify-content: center;" style="margin-left: 30%;">
+            <form>
+                <input type="text" name="name" placeholder="Nombre Completo">
+                <input type="text" name="tel" placeholder="N-Telefono">
+                <input type="submit" name="submit" value="AgregarLST" />
+                <?php
+                foreach ($lista as $name => $tel) {
+                    echo '<input type="hidden" name="lista[' . $name . ']" value="' . $tel . '">';
+                } ?>
+            </form>
+
+            <?php
+            //Para acabar Imprimiremos la lista de contactos recorriendo el array
+            echo "<ol>";
+            foreach ($lista as $name => $tel) {
+                echo "<li ><p>$name --> $tel</p></li>";
+            }
+            echo '</ol>';
+            ?>
+
+
+
+        </div>
 </body>
+
 </html>
