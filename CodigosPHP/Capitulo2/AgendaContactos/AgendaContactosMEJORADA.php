f7113d4433f1dd6e0b75d4d44eef2d14d94c2166
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-11-03 16:07:29 +0100

    Session save check

--- CodigosPHP/Capitulo2/AgendaContactos/AgendaContactosMEJORADA.php
+++ CodigosPHP/Capitulo2/AgendaContactos/AgendaContactosMEJORADA.php
@@ -1,6 +1,12 @@
+<?php session_start();?>
 <!DOCTYPE html>
+<!-- Cookies - session_start() antes de HTML  -->
+<!-- http://idesweb.es/proyecto/proyecto-prac09-php-cookies-sesiones -->
+<!--  https://www.w3schools.com/php/php_cookies.asp -->
 <head></head>
 <body>
-    
+    <?php
+    echo ini_get("session.session_save_path");
+    ?>
 </body>
 </html>
